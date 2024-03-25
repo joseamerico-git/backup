88215c9d98691c6fc93dc224e859a67519bebc72
JOSÉ AMERICO DOICHE JUNIOR <jose.junior@CARTORIOTABNOTA.local>
2024-03-25 17:01:15 -0300

    criando telas

--- server/main.js
+++ server/main.js
@@ -4,11 +4,15 @@ var select = document.getElementById('categorias');
 const categoria = "todos";
 const container_produtos = document.getElementById('container_produtos')
 const select_categorias = document.getElementById('select_categorias')
+const token ='';
+const logado = document.getElementById('logado')
 
 
+
+           
 
 function getAllProducts() {
-    const allData = async() => {
+    const allData = async () => {
         const response = await axios.get("http://localhost:3001/api/produtos");
         return response.data; // retorna apenas o array de Json
     }
@@ -17,7 +21,7 @@ function getAllProducts() {
     allData().then((data) => {
         // aqui dentro, a variável 'data' contém o array de Json retornado pela Promise
         console.log(data.listProdutos)
-            // console.log(data.listProdutos[0].imagens[0].nome)
+        // console.log(data.listProdutos[0].imagens[0].nome)
         const url = null;
         data.listProdutos.forEach((produto) => {
             listImagens = produto.imagens;
@@ -35,7 +39,7 @@ function getAllProducts() {
 }
 
 function getAllProductsByCategoria(categoriaId) {
-    const allData = async() => {
+    const allData = async () => {
         const response = await axios.get(`http://localhost:3001/api/produtos/cat/${categoriaId}`);
         return response.data; // retorna apenas o array de Json
     }
@@ -44,7 +48,7 @@ function getAllProductsByCategoria(categoriaId) {
     allData().then((data) => {
         // aqui dentro, a variável 'data' contém o array de Json retornado pela Promise
         console.log(data.listProdutos)
-            // console.log(data.listProdutos[0].imagens[0].nome)
+        // console.log(data.listProdutos[0].imagens[0].nome)
         const url = null;
         data.listProdutos.forEach((produto) => {
             listImagens = produto.imagens;
@@ -137,7 +141,7 @@ function selecionandoCategorias() {
 }
 
 function getAllCategorias() {
-    const allData = async() => {
+    const allData = async () => {
         const response = await axios.get("http://localhost:3001/api/categorias");
         return response.data; // retorna apenas o array de Json
     }
@@ -146,12 +150,12 @@ function getAllCategorias() {
     allData().then((data) => {
         // aqui dentro, a variável 'data' contém o array de Json retornado pela Promise
         console.log(data.listCategorias)
-            // console.log(data.listProdutos[0].imagens[0].nome)
-            // const url = null;
-            // data.listProdutos.forEach((produto) => {
-            //  listImagens = produto.imagens;
-            // listImagens.forEach((imagem) => {
-            //  adicionaImagem1(imagem.url, produto)
+        // console.log(data.listProdutos[0].imagens[0].nome)
+        // const url = null;
+        // data.listProdutos.forEach((produto) => {
+        //  listImagens = produto.imagens;
+        // listImagens.forEach((imagem) => {
+        //  adicionaImagem1(imagem.url, produto)
 
 
         //adicionaImagem(`http://localhost:3001/files/${produto.nome}`)
@@ -181,9 +185,226 @@ getAllCategorias();
 
 function carregaCategorias(categoria) {
 
-
     option.textContent = categoria.descricao;
     option.value = categoria.id;
-
     select_categorias.appendChild(option);
+}
+
+function getAllProdutosLike(nome) {
+    const allData = async () => {
+        const response = await axios.get(`http://localhost:3001/api/produtos/${nome}`);
+        return response.data; // retorna apenas o array de Json
+    }
+
+    // para utilizar o resultado da Promise, é possível fazer o seguinte:
+    allData().then((data) => {
+        // aqui dentro, a variável 'data' contém o array de Json retornado pela Promise
+        console.log(data.listCategorias)
+        // console.log(data.listProdutos[0].imagens[0].nome)
+        // const url = null;
+        // data.listProdutos.forEach((produto) => {
+        //  listImagens = produto.imagens;
+        // listImagens.forEach((imagem) => {
+        //  adicionaImagem1(imagem.url, produto)
+
+
+        //adicionaImagem(`http://localhost:3001/files/${produto.nome}`)
+        const listCategorias = data.listCategorias;
+
+        listCategorias.forEach((categoria => {
+
+            console.log(categoria)
+            const option = document.createElement('option');
+            option.value = categoria.id;
+            option.innerText = categoria.descricao;
+            select_categorias.appendChild(option)
+            console.log(categoria.id)
+
+
+        }))
+
+
+
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+
+function cadastrarCategoria() {
+    const form = document.querySelector('cadCategoria');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const descricao = document.getElementById("descr").value;
+
+        const newCategoria = {
+            descricao: descricao
+        }
+
+
+
+        fetch('/categorias', {
+            method: 'POST',
+            headers: {
+                //  'Content-Type': 'multipart/form-data'
+            },
+            body: newCategoria
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Falhou!');
+                }
+                console.log('Sucesso no upload!');
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+
+    });
+}
+
+console.log('token '+localStorage.getItem('token'))
+
+function cadastroProdutos() {
+    const form = document.querySelector('cadCategoria');
+   
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const nome = document.getElementById("nome").value;
+        const descricao = document.getElementById("descricao").value;
+        const estoque = document.getElementById("estoque").value;
+
+
+
+        const newProduct = {
+            nome: nome,
+            descricao: descricao,
+            estoque: estoque,
+            categoriaId:1
+          
+
+        }
+
+
+
+        fetch('/produtos', {
+            method: 'POST',
+            headers: {
+                //  'Content-Type': 'multipart/form-data'
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFkbWluIiwiaWF0IjoxNzExMzk0NTk3fQ.iKTJ9U-U_sNPt8-i90KS41iR4k4-K4fMvNipc7EMRRY"
+            },
+            body: newProduct
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    
+                    throw new Error('Falhou!');
+                }
+                console.log('Sucesso no cadastro!');
+            })
+            .catch((error) => {
+                console.error(error);
+               
+            });
+
+
+    });
+}
+
+function addUser() {
+    const form = document.querySelector("addUser");
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const username = documet.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        const newUser = {
+            username: username,
+            password: password
+        }
+
+
+        function validaForm() {
+            if (newUser) {
+                return true;
+            } else {
+                console.log("nenhum usuário foi preenchido!")
+                return false;
+            }
+
+
+        }
+
+
+        if (validaForm()) {
+            fetch('/users', {
+                method: 'POST',
+                headers: {
+
+                },
+                body: newUser
+
+
+
+            }).then((response) => {
+                if (!response.ok) {
+                    throw new Error('Falhou!');
+                }
+                console.log('Sucesso no upload!');
+            })
+                .catch((error) => {
+                    console.error(error);
+                });
+
+        }
+
+    })
+
+
+}
+
+
+function login() {
+    form = document.getElementById("login")
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const username = documet.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        const userLogin = {
+            username: username,
+            password: password
+        }
+
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+
+            },
+            body: userLogin
+        }).then((response)=>{
+            if(!response.ok){
+                throw new Error('Não foi possível realizar o login! Verifique as credênciais!');
+          
+            }
+            
+           
+           token = response;
+           localStorage.setItem('token',response);
+           meuToken = response;
+
+
+        }).catch((error)=>{
+            console.log(error)
+        })
+
+    })
 }
